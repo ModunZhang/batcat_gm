fb191755a2cbbcf40084b1f843d168125a089741
Modun Zhang <modunzhang@gmail.com>
2015-12-11 16:27:02 +0800

    新增游戏内基本状态严控

--- app/controllers/manager.js
+++ app/controllers/manager.js
@@ -4,9 +4,13 @@
 
 var express = require('express');
 var _ = require('underscore');
+var mongoose = require('mongoose');
 
 var auth = require('../../middlewares/authorization');
 var consts = require('../../config/consts');
+var utils = require('../../config/utils');
+
+var Game = mongoose.model('Game');
 
 var router = express.Router();
 module.exports = router;
@@ -15,4 +19,39 @@ router.all('*', auth.requiresLogin, auth.requiresUserRight.bind(auth, consts.Use
 
 router.get('/', function(req, res){
   res.render('manager/index');
+});
+
+router.param('gameId', function(req, res, next, gameId){
+  Game.findById(gameId).then(function(game){
+    if(!game) return next(new Error('Game not exist'));
+    req.game = game;
+    next();
+  }, function(e){
+    next(e);
+  })
+});
+
+router.get('/games/list', function(req, res, next){
+  Game.find({}, 'name ip port').then(function(games){
+    res.render('manager/games/list', {games:games});
+  }, function(e){
+    next(e);
+  });
+});
+
+router.get('/games/get-server-info/:gameId', function(req, res, next){
+  var game = req.game;
+  utils.get(game.ip, game.port, 'get-servers-info', {}, function(e, data){
+    if(!!e) next(e);
+    var serverNames = _.sortBy(_.keys(data), function(serverName){
+      return serverName;
+    });
+    var servers = [];
+    _.each(serverNames, function(serverName){
+      var server = data[serverName];
+      server.name = serverName;
+      servers.push(server);
+    });
+    res.render('manager/games/get-server-info', {game:game, servers:servers});
+  });
 });
