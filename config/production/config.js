7ea4b4d6efee7e8ff71f7d4edf3ee1b5fb71fad9
Modun Zhang <modunzhang@gmail.com>
2016-10-17 16:34:22 +0800

    更新数据库地址

--- config/production/config.js
+++ config/production/config.js
@@ -4,5 +4,5 @@
 
 module.exports = {
   sessionSecret:'wzuqbNlhSFNjv0RUSTLZ',
-  mongoHost: 'mongodb://modun:Zxm75504109@10.45.22.67:27017/batcat_gm?authSource=admin'
+  mongoHost: 'mongodb://modun:Zxm75504109@10.24.233.192:27017/batcat_gm?authSource=admin'
 };
